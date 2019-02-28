ac34bf358c8b38b3ae9c7afb4fe4fed2718c5e77
joevillany <villanyc0@gmail.com>
2019-02-27 19:36:01 -0500

    Added React app boilerplate counter

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={"https://app.feedbackpanda.com/images/fbp_logo_circle.png"} className="App-logo" alt="logo" />
+          <p>
+            YOOO <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
